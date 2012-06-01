3627ee61883e3670d1d1296eca378ab4980b7c48
Reuven V. Gonzales <reuven@tobetter.us>
2012-05-31 18:17:17 -0700

    Added and tested manager

--- lxc4u/lxc.py
+++ lxc4u/lxc.py
@@ -8,26 +8,34 @@ class LXCAlreadyStarted(Exception):
         message = 'LXC named "%s" is already running' % name
         super(LXCAlreadyStarted, self).__init__(message)
 
+class LXCDoesNotExist(Exception):
+    def __init__(self, name):
+        message = 'LXC named "%s" does not exist' % name
+        super(LXCDoesNotExist, self).__init__(message)
+
 class LXC(object):
     @classmethod
-    def create(cls, name, template="ubuntu"):
+    def create(cls, name, template="ubuntu", service=None):
         """Create a brand new LXC"""
-        LXCService.create(name, template=template)
-        return cls(name)
+        service = service or LXCService
+        service.create(name, template=template)
+        return cls(name, service=service)
 
     @classmethod
-    def create_with_overlays(cls, name, base, overlays, overlay_temp_path=None):
+    def create_with_overlays(cls, name, base, overlays, overlay_temp_path=None,
+            service=None): 
         """Creates an LXC using overlays. 
         
         This is a fast process in comparison to LXC.create because it does not
         involve any real copying of data.
         """
+        service = service or LXCService
         # Check that overlays has content
         if not overlays:
             raise TypeError("Argument 'overlays' must have at least one item")
 
         # Get the system's LXC path
-        lxc_path = LXCService.lxc_path()
+        lxc_path = service.lxc_path()
         # Calculate base LXC's path
         base_path = os.path.join(lxc_path, base)
         # Calculate the new LXC's path
@@ -59,11 +67,14 @@ class LXC(object):
     def from_name(cls, name, service=None):
         # Seems redundant now, but I prefer this over calling the constructor
         # directly. This could improve later
+        service = service or LXCService
+        if not name in service.list_names():
+            raise LXCDoesNotExist(name)
         return cls(name, service=service)
 
     def __init__(self, name, service=None):
         self.name = name
-        self._service = service or LXCService
+        self._service = service
 
     def start(self):
         """Start this LXC"""
@@ -77,10 +88,22 @@ class LXC(object):
 
     @property
     def status(self):
-        info = self._service.info()
-        return info['status']
+        info = self._service.info(self.name)
+        return info['state']
     
     @property
     def pid(self):
-        info = self._service.info()
+        info = self._service.info(self.name)
         return int(info['pid'])
+
+class LXCManager(object):
+    @classmethod
+    def list(cls, service=None):
+        """Get's all of the LXC's and creates objects for them"""
+        service = service or LXCService
+        lxc_names = service.list_names()
+        return map(lambda name: LXC(name, service=service), lxc_names)
+
+    @classmethod
+    def get(cls, name, service=None):
+        return LXC.from_name(name, service=service)
--- tests/test_lxc.py
+++ tests/test_lxc.py
@@ -1,7 +1,8 @@
-import fudge
 from nose.tools import raises
+import fudge
+from fudge.inspector import arg
 import testkit
-from lxc4u.lxc import LXC, LXCAlreadyStarted
+from lxc4u.lxc import *
 
 @fudge.patch('lxc4u.lxc.LXCService')
 def test_create_a_container(fake_service):
@@ -11,15 +12,23 @@ def test_create_a_container(fake_service):
     message = "test1_lxc isn't an LXC instance"
     assert isinstance(test1_lxc, LXC) == True, message
 
-def test_container_from_name():
+@fudge.patch('lxc4u.lxc.LXCService')
+def test_container_from_name(fake_service):
+    fake_service.expects('list_names').returns(['name'])
     test1_lxc = LXC.from_name('name')
     message = "test1_lxc isn't an LXC instance"
     assert isinstance(test1_lxc, LXC) == True, message
 
+@raises(LXCDoesNotExist)
+@fudge.patch('lxc4u.lxc.LXCService')
+def test_container_from_name_does_not_exist(fake_service):
+    fake_service.expects('list_names').returns([])
+    LXC.from_name('name')
+
 class TestLXC(object):
     def setup(self):
         self.fake_service = fudge.Fake('LXCService')
-        self.lxc = LXC.from_name('name', service=self.fake_service)
+        self.lxc = LXC('name', service=self.fake_service)
 
     @fudge.test
     def test_start_container(self):
@@ -89,3 +98,15 @@ class TestLXCWithOverlay(object):
                 base='test1', overlays=['overlay1_path', 'overlay2_path'])
         message = "test1_lxc_overlay isn't an LXC instance"
         assert isinstance(test1_overlay_lxc, LXC) == True, message
+
+@fudge.patch('lxc4u.lxc.LXCService')
+def test_lxc_manager_list(fake_service):
+    fake_service.expects('list_names').returns(['lxc1', 'lxc2'])
+    lxc_list = LXCManager.list()
+    for lxc in lxc_list:
+        assert isinstance(lxc, LXC)
+
+@fudge.patch('lxc4u.lxc.LXC')
+def test_lxc_manager_get(fake_lxc_class):
+    fake_lxc_class.expects('from_name').with_args('name', service=arg.any()).returns('LXC')
+    assert LXCManager.get('name') == 'LXC'
