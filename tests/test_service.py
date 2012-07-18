6c254be7572c3dbbe5931479a2e77025313a92f5
Reuven V. Gonzales <reuven@tobetter.us>
2012-07-18 15:17:38 -0700

    test_service updated to use mock

--- tests/test_service.py
+++ tests/test_service.py
@@ -1,4 +1,5 @@
 import fudge
+from mock import patch
 from lxc4u.service import LXCService
 
 def test_service_list_names():
@@ -11,38 +12,50 @@ def test_service_list_names():
     for ls_str, expected in tests:
         yield do_service_list_names, ls_str, expected
 
-@fudge.patch('subwrap.run')
-def do_service_list_names(ls_str, expected, fake_run):
-    fake_resp = fake_run.expects_call().with_args(['lxc-ls']).returns_fake()
-    fake_resp.has_attr(std_out=ls_str)
+@patch('subwrap.run')
+def do_service_list_names(ls_str, expected, mock_run):
+    # Setup Mocks
+    mock_resp = mock_run.return_value
+    mock_resp.std_out = ls_str
+    mock_resp.has_attr(std_out=ls_str)
+    
+    # Run Test
     lxc_list = LXCService.list_names()
+
+    # Assertions
+    mock_run.assert_called_with(['lxc-ls'])
     assert lxc_list == expected
 
-@fudge.patch('subwrap.run')
-def test_service_create(fake_run):
-    fake_run.expects_call()
+@patch('subwrap.run')
+def test_service_create(mock_run):
     LXCService.create('something')
+    mock_run.assert_any_calls()
 
-@fudge.patch('subwrap.run')
-def test_service_start(fake_run):
-    fake_run.expects_call()
+@patch('subwrap.run')
+def test_service_start(mock_run):
     LXCService.start('something')
+    mock_run.assert_any_calls()
 
-@fudge.patch('subwrap.run')
-def test_service_stop(fake_run):
-    fake_run.expects_call()
+@patch('subwrap.run')
+def test_service_stop(mock_run):
     LXCService.stop('something')
+    mock_run.assert_any_calls()
 
-@fudge.patch('subwrap.run')
-def test_service_destroy(fake_run):
-    fake_run.expects_call()
+@patch('subwrap.run')
+def test_service_destroy(mock_run):
     LXCService.destroy('something')
+    mock_run.assert_any_calls()
 
-@fudge.patch('subwrap.run')
-def test_service_info(fake_run):
-    fake_resp = fake_run.expects_call().returns_fake()
-    fake_resp.has_attr(std_out="state:   RUNNING\npid:    12345")
+@patch('subwrap.run')
+def test_service_info(mock_run):
+    # Setup Mocks
+    mock_resp = mock_run.return_value
+    mock_resp.std_out = "state:   RUNNING\npid:    12345"
+
+    # Run Test
     info = LXCService.info('something')
+
+    # Assertions
     assert info == {"state": "RUNNING", "pid": "12345"}
 
 def test_many_service_info():
@@ -53,17 +66,22 @@ def test_many_service_info():
     for info_str, expected in tests:
         yield do_service_info, info_str, expected
 
-@fudge.patch('subwrap.run')
-def do_service_info(info_str, expected, fake_run):
-    fake_resp = fake_run.expects_call().returns_fake()
-    fake_resp.has_attr(std_out=info_str)
+@patch('subwrap.run')
+def do_service_info(info_str, expected, mock_run):
+    # Setup Mocks
+    mock_resp = mock_run.return_value
+    mock_resp.std_out = info_str
+
+    # Run Test
     info = LXCService.info("something")
+
+    # Assertions
     assert info == expected
 
-@fudge.patch('subwrap.run')
-def test_service_lxc_path(fake_run):
-    fake_resp = fake_run.expects_call().returns_fake()
-    fake_resp.has_attr(std_out='/var/lib/lxc\n')
+@patch('subwrap.run')
+def test_service_lxc_path(mock_run):
+    mock_resp = mock_run.return_value
+    mock_resp.std_out = '/var/lib/lxc\n'
+
     assert LXCService.lxc_path() == '/var/lib/lxc'
     assert LXCService.lxc_path('hello') == '/var/lib/lxc/hello'
-
