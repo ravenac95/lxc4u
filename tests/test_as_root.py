03b03e98992675abd55c2a5dd3bf0cd26e190792
Reuven V. Gonzales <reuven@tobetter.us>
2012-07-19 15:06:09 -0700

    Large tests both functioning now!
    Considering a 0.0.1 release

--- tests/test_as_root.py
+++ tests/test_as_root.py
@@ -12,11 +12,13 @@ do not run these tests in a production system.
 import tempfile
 import shutil
 import os
+import subwrap
 from nose.plugins.attrib import attr
 from testkit import random_string, temp_directory
 from testkit.data import ALPHAS_LOWER
 from .utils import only_as_root
 import lxc4u
+
 
 def read_file_data(filepath):
     f = open(filepath)
@@ -24,11 +26,12 @@ def read_file_data(filepath):
     f.close()
     return data
 
+
 @only_as_root
 @attr('large')
 def test_create():
     """Create a new LXC"""
-    # WARNING this test assumes that you're using /var/lib/lxc for 
+    # WARNING this test assumes that you're using /var/lib/lxc for
     # lxc containers. If that is not correct please do not run this test
     # Create a random directory path for the test container
     container_path = tempfile.mkdtemp(dir="/var/lib/lxc")
@@ -38,11 +41,15 @@ def test_create():
     random_name = os.path.basename(container_path)
     try:
         # Create the new container
-        lxc4u.create(random_name)
+        lxc = lxc4u.create(random_name)
         # Assert that the new container was created at the correct path
         assert os.path.exists(container_path) == True, "Container not created"
         # Assert that it's a directory
         assert os.path.isdir(container_path) == True, "Container not a directory"
+        lxc.destroy()
+
+        # Check that the clean up did it's job
+        assert os.path.exists(container_path) == False
     finally:
         # Always delete the temp directory if it still exists
         if os.path.exists(container_path):
@@ -51,24 +58,68 @@ def test_create():
             else:
                 os.remove(container_path)
 
-#@only_as_root
-#@attr('large')
-#def test_create_with_overlay():
-#    """Create a new LXC with a temp overlay"""
-#    random_name = random_string(26, ALPHAS_LOWER)
-#    # Create a random directory path for the test container
-#    container_path = tempfile.mkdtemp(dir='/var/lib/lxc')
-#    # Delete path that was created
-#    shutil.rmtree(container_path)
-#    # Get random name using temp directory path
-#    random_name = os.path.basename(container_path)
-#    with temp_directory() as temp_path:
-#        try:
-#            # FIXME it assumes you have an LXC named base
-#            lxc4u.create(random_name, base="base", overlays=[temp_path])
-#        finally:
-#            if os.path.exists(container_path):
-#                if os.path.isdir(container_path):
-#                    shutil.rmtree(container_path)
-#                else:
-#                    os.remove(container_path)
+
+@only_as_root
+@attr('large')
+def test_create_with_overlay():
+    """Create a new LXC with a temp overlay"""
+    random_name = random_string(26, ALPHAS_LOWER)
+    # Create a random directory path for the test container
+    container_path = tempfile.mkdtemp(dir='/var/lib/lxc')
+    # Delete path that was created
+    shutil.rmtree(container_path)
+    # Get random name using temp directory path
+    random_name = os.path.basename(container_path)
+    with temp_directory() as temp_path:
+        overlay_group = None
+        try:
+            # FIXME it assumes you have an LXC named base
+            overlay_lxc = lxc4u.create(random_name, base="base",
+                    overlays=[temp_path])
+
+            overlay_group = overlay_lxc._overlay_group
+
+            # Assert that the new container was created at the correct path
+            assert os.path.exists(container_path) == True, "Container not created"
+            # Assert that it's a directory
+            assert os.path.isdir(container_path) == True, "Container not a directory"
+
+            # Attempt to clean up
+            overlay_lxc.destroy()
+
+            # Check that the clean up did it's job
+            assert os.path.exists(container_path) == False
+        finally:
+            # FIXME clean up isn't a for sure thing here :-/
+            if overlay_group:
+                unmount_overlays(overlay_group)
+            else:
+                unmount_mount_point(container_path)
+            if os.path.exists(container_path):
+                if os.path.isdir(container_path):
+                    shutil.rmtree(container_path)
+                else:
+                    os.remove(container_path)
+
+
+def unmount_overlays(overlays):
+    for overlay in overlays:
+        try:
+            overlay.unmount()
+        except subwrap.CommandError, e:
+            print ('Attempted to unmount %s, but got:' %
+                    overlay.mount_point)
+            print e.response.std_err
+
+
+def unmount_mount_points(points):
+    for point in points:
+        unmount_mount_point(point)
+
+
+def unmount_mount_point(point):
+    try:
+        subwrap.run(['umount', point])
+    except subwrap.CommandError, e:
+        print 'Attempted to unmount %s, but got:' % point
+        print e.response.std_err
